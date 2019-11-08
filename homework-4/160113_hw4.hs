e10e9f4bb77976cdf5b5ed79569015123e5b86fe
Aniket Pandey <aniketp@iitk.ac.in>
2019-11-08 16:05:31 +0530

    Homework 4: Initial commit (Q.1)

--- homework-4/160113_hw4.hs
+++ homework-4/160113_hw4.hs
@@ -2,3 +2,37 @@
  - Homework 4 :: CS350A 2019-20 I
  - Author: Aniket Pandey (160113)
  -}
+
+{-permute :: (Num a) => [a] -> [b] -> [a]-}
+{-permute _ _ = []-}
+{-permute (x:xs) (y:ys) =  -}
+
+compose :: [(a -> a)] -> a -> a
+compose [] x = x
+compose (x:xs) y = compose xs (x y)
+
+altMap :: [a] -> (a -> b) -> (a -> b) -> [b]
+altMap (fst:snd:rest) f g = (f fst):(g snd): altMap rest f g
+altMap [x] f g = [f x]
+altMap _ _ _ = []
+
+data BinaryTree a = Nil | Node a (BinaryTree a) (BinaryTree a)
+                        deriving (Eq, Show, Read)
+
+binaryTreeMap :: (a -> b) -> BinaryTree a -> BinaryTree b
+binaryTreeMap f Nil = Nil
+binaryTreeMap f (Node a left right) = Node (f a) (binaryTreeMap f left) (binaryTreeMap f right)
+
+instance Functor BinaryTree where
+    fmap = binaryTreeMap
+
+instance Applicative BinaryTree where
+    pure x = Node x Nil Nil
+    Nil <*> _ = Nil
+    _ <*> Nil = Nil
+    Node f left right <*> Node x left' right' = Node (f x) (left <*> left') (right <*> right')
+
+instance Monad BinaryTree where
+    return x = Node x Nil Nil
+    Nil >>= _ = Nil
+    Node x left right >>= f = f x
